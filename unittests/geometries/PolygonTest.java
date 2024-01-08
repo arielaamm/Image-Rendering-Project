3cd6bf6370d7d53e7a9a2f85956f76bc760d1db5
YairBybabayov <bybabayo@g.jct.ac.il>
2024-01-08 14:08:12 +0200

    PointTest tests

---
+++ unittests/geometries/PolygonTest.java
@@ -0,0 +1,14 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PolygonTest {
+    /**
+     * Unit tests for geometries.Polygon class
+     */
+    @Test
+    void getNormal() {
+    }
+}
---
+++ unittests/geometries/RadialGeometryTest.java
@@ -0,0 +1,9 @@
+package geometries;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Unit tests for geometries.RadialGeometry class
+ */
+class RadialGeometryTest {
+
+}
